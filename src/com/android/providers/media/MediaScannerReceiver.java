3874ece6a4230832d960146dd9717a4b98a687b2
liuwei <liuwei@hisensecom.com>
2013-05-16 14:32:09 +0800

    revert to last version

--- src/com/android/providers/media/MediaScannerReceiver.java
+++ src/com/android/providers/media/MediaScannerReceiver.java
@@ -24,9 +24,6 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
 import android.util.Log;
-import android.telephony.TelephonyManager;
-import android.content.pm.PackageManager;
-import android.content.ComponentName;
 
 public class MediaScannerReceiver extends BroadcastReceiver {
     private final static String TAG = "MediaScannerReceiver";
@@ -39,20 +36,6 @@ public class MediaScannerReceiver extends BroadcastReceiver {
             // Scan both internal and external storage
             scan(context, MediaProvider.INTERNAL_VOLUME);
             //scan(context, MediaProvider.EXTERNAL_VOLUME);//because InternalStorage may be unmounted after BOOT_COMPLETED 
-
-            if (!TelephonyManager.isMultiSimEnabled())
-            {
-               PackageManager pm = context.getPackageManager();
-               pm.setComponentEnabledSetting(new ComponentName("com.android.phone",
-                                             "com.android.phone.MSimCallFeaturesSubSetting"),
-                                             PackageManager.COMPONENT_ENABLED_STATE_DISABLED, 0);
-            }else{
-                PackageManager pm = context.getPackageManager();
-                pm.setComponentEnabledSetting(new ComponentName("com.android.phone",
-                                              "com.android.phone.CallFeaturesSetting"),
-                                              PackageManager.COMPONENT_ENABLED_STATE_DISABLED, 0);
-            }
-
         } else {
             if (uri.getScheme().equals("file")) {
                 // handle intents related to external storage
