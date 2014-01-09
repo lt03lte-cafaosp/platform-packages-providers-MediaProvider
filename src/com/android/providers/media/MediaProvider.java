4e1d9aee3dbb2f3a5f4ed5e29fbe128fc3ad2b37
guopingy <guopingy@codeaurora.org>
2014-01-09 14:00:01 +0800

    Fix CR583021: Screenshots info is not updated when device is plugged in MTP mode.
    Change-Id: Ifd5cc6e80810177f456d62d281bad1beb1d1f336

--- src/com/android/providers/media/MediaProvider.java
+++ src/com/android/providers/media/MediaProvider.java
@@ -2723,6 +2723,19 @@ public class MediaProvider extends ContentProvider {
         }
     }
 
+    private void sendObjectUpdated(long objectHandle) {
+        synchronized (mMtpServiceConnection) {
+            if (mMtpService != null) {
+                try {
+                    mMtpService.sendObjectUpdated((int)objectHandle);
+                } catch (RemoteException e) {
+                    Log.e(TAG, "RemoteException in sendObjectUpdated", e);
+                    mMtpService = null;
+                }
+            }
+        }
+    }
+
     @Override
     public int bulkInsert(Uri uri, ContentValues values[]) {
         int match = URI_MATCHER.match(uri);
@@ -4239,6 +4252,11 @@ public class MediaProvider extends ContentProvider {
                                         + count + " match = " + match);
                             }
                         }
+                        if (count > 0) {
+                           helper.mNumQueries++;
+                           notifyMtpUpdated(sGetTableAndWhereParam.table, db,
+                               sGetTableAndWhereParam.where, whereArgs);
+                        }
                     }
                     break;
                 case IMAGES_MEDIA:
@@ -4283,6 +4301,12 @@ public class MediaProvider extends ContentProvider {
                                 }
                             }
                         }
+
+                        if (count > 0) {
+                            helper.mNumQueries++;
+                            notifyMtpUpdated(sGetTableAndWhereParam.table, db,
+                                    sGetTableAndWhereParam.where, whereArgs);
+                        }
                     }
                     break;
 
@@ -4305,6 +4329,11 @@ public class MediaProvider extends ContentProvider {
                     helper.mNumUpdates++;
                     count = db.update(sGetTableAndWhereParam.table, initialValues,
                         sGetTableAndWhereParam.where, whereArgs);
+                    if (count > 0) {
+                        helper.mNumQueries++;
+                        notifyMtpUpdated(sGetTableAndWhereParam.table, db,
+                                sGetTableAndWhereParam.where, whereArgs);
+                    }
                     break;
             }
         }
@@ -4314,6 +4343,20 @@ public class MediaProvider extends ContentProvider {
             getContext().getContentResolver().notifyChange(uri, null);
         }
         return count;
+    }
+
+    private void notifyMtpUpdated(String table, SQLiteDatabase db,
+            String userWhere, String[] whereArgs) {
+        Cursor c = db.query(table, ID_PROJECTION, userWhere, whereArgs, null, null, null);
+        if (c != null) {
+            try {
+                while (c.moveToNext()) {
+                    sendObjectUpdated(c.getLong(0));
+                }
+            } finally {
+                c.close();
+            }
+        }
     }
 
     private int movePlaylistEntry(DatabaseHelper helper, SQLiteDatabase db,
